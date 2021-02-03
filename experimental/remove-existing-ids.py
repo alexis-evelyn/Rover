4ac9574a81f19e38163a4d60d9ca6fcc1f351db4
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-02-03 03:38:27 -0500

    Renamed and Fixed Remove Existing Ids Script

--- experimental/remove-existing-ids.py
+++ experimental/remove-existing-ids.py
@@ -2,14 +2,14 @@
 
 import pandas as pd
 
-existing_tweets: pd.DataFrame = pd.read_csv("working/missing/trump-existing.csv")
-tweets_list: pd.DataFrame = pd.read_csv("working/missing/trump-sorted.csv")
+existing_tweets: pd.DataFrame = pd.read_csv("working/missing/existing-ids.csv")
+tweets_list: pd.DataFrame = pd.read_csv("working/missing/tweet-ids.csv")
 
 existing: list = existing_tweets["id"].to_list()
 
 temp: pd.DataFrame = tweets_list.loc[~(tweets_list["id"].isin(existing))]
 temp.reset_index(drop=True, inplace=True)
 
-temp.to_csv("working/presidential-tweets/trump-download.csv")
+temp.to_csv("working/missing/total-download.csv", index=False)
 
 print(temp)

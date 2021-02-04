1e0f1ff88b0acec459f9e23938413b5ec996ba9c
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-02-04 01:29:18 -0500

    Cleanup Wayback Data

--- experimental/wayback/cleanup_wayback_data.py
+++ experimental/wayback/cleanup_wayback_data.py
@@ -2,3 +2,11 @@
 
 import pandas as pd
 
+matches_file: str = "working/wayback-matches.csv"
+matches_file_clean: str = "working/wayback-matches-clean.csv"
+
+matches: pd.DataFrame = pd.read_csv(matches_file, index_col=["id"])
+matches = matches.groupby(matches.index).first()
+
+print(matches)
+matches.to_csv(matches_file_clean)

ec9189a9f2176b603a7749f8e1891f950ea38975
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-02-06 02:16:12 -0500

    Experiment With HTTP 2

--- experimental/http2/http2.py
+++ experimental/http2/http2.py
@@ -1,25 +1,36 @@
+#!/bin/python3
+
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+from starlette.requests import Request
 
 import asyncio
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
 
-async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+async def homepage(request: Request):
+    return JSONResponse({
+        'path': str(request.url.path),
+        'query': str(request.url.query)
+    })
 
 
 if __name__ == '__main__':
     # TODO: Figure Out How To Get Truly Dynamic URLs
     # https://www.starlette.io/routing/
     routes = [
-        Route("/", endpoint=homepage)
+        Route("/{path:path}", endpoint=homepage, methods=["GET", "POST"])
     ]
 
-    app = Starlette(debug=True, routes=routes)
+    config: Config = Config()
+    config.bind = ["localhost:8930"]
+    config.certfile = "working/certs/cert.pem"
+    config.keyfile = "working/certs/key.pem"
 
-    # TODO: Figure Out How To Use These Settings In App
-    # hypercorn --certfile working/certs/cert.pem --keyfile working/certs/key.pem --bind localhost:8000 experimental/http2:app
-    # asyncio.run(serve(app, Config()))
+    app = Starlette(debug=True, routes=routes)
+    asyncio.run(serve(app=app, config=config))
+
+    # Cannot Work Without Disabling __main__ check
+    # hypercorn --certfile working/certs/cert.pem --keyfile working/certs/key.pem --bind localhost:8930 experimental/http2/http2:app

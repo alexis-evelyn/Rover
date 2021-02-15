c8f340388017b84e1692b08e3c4527100c086d1c
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-02-15 12:26:40 -0500

    Testing Hotfix For Firefox

--- rover/server/web/scripts/helper.js
+++ rover/server/web/scripts/helper.js
@@ -4,8 +4,8 @@ overrideScrollReload()
 
 // TODO: Add Means To Delete All Cache and Service Worker!!!
 
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function convertDateToLocalString(dateNum) {
     // let temp = Date.parse("2021-01-26 13:20:46 +0000 UTC")

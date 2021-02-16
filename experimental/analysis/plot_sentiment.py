984deecf775302228d61706b3238d5520e23eb0f
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-02-15 23:03:01 -0500

    Saving Current Work

--- experimental/analysis/plot_sentiment.py
+++ experimental/analysis/plot_sentiment.py
@@ -62,12 +62,44 @@ def get_dataframe(engine: sqlalchemy.engine) -> pd.DataFrame:
     # This tosses any rows that don't share the same index on both DataFrames
     merged = pd.merge(tweets, analysis, left_index=True, right_index=True)
 
-    print("Displaying Preview of DataFrame")
-    print(merged)
+    # print("Displaying Preview of DataFrame")
+    # print(merged)
     return merged
 
 
+def drop_unwanted_candidates(data: pd.DataFrame) -> pd.DataFrame:
+    # Keep realDonaldTrump (25073877), JoeBiden (939091), and POTUS44 (1536791610)
+    print("Keeping Only Desired Candidates")
+    keep: list = ["realDonaldTrump", "JoeBiden", "POTUS44"]  # [25073877, 939091, 1536791610] for twitter_user_id
+    data = data[data["twitter_handle"].isin(keep)]
+
+    print("Displaying Preview of DataFrame")
+    print(data)
+    return data
+
+
+def average_points(data: pd.DataFrame) -> pd.DataFrame:
+    # TODO: Average Polarity By Day By Candidate
+    # TODO: Average Subjectivity By Day By Candidate
+    print("Averaging Points")
+    handles: list = data["twitter_handle"].unique().tolist()
+    rows: dict = {}
+
+    for handle in handles:
+        rows[handle] = data.loc[data["twitter_handle"] == handle]
+
+    for row in rows:
+        print("{handle} Polarity: {polarity}".format(handle=row, polarity=rows[row]["polarity"].mean()))
+        print("{handle} Subjectivity: {subjectivity}".format(handle=row, subjectivity=rows[row]["subjectivity"].mean()))
+
+    pass
+
+
 def draw_plot(data: pd.DataFrame):
+    # https://pandas.pydata.org/docs/getting_started/intro_tutorials/04_plotting.html
+    # TODO: Draw Average Polarity With Solid Line (Different Color Per President)
+    # TODO: Draw Average Subjectivity With Shaded Line (Different Color Per President)
+    print("Plotting Graph")
     pass
 
 
@@ -75,4 +107,8 @@ if __name__ == "__main__":
     connection = start_server()
     time.sleep(1)
     merged = get_dataframe(engine=connection)
-    draw_plot(data=merged)
+
+    cleaned = drop_unwanted_candidates(data=merged)
+    averaged = average_points(data=cleaned)
+    draw_plot(data=averaged)
+    print("Done")
